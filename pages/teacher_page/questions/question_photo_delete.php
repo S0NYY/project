67f795367465de2863382f582928574215cd32a3
S0NYY <mrtetteo@gmail.com>
2021-06-23 02:28:50 +0400

    edit

--- pages/teacher_page/questions/question_photo_delete.php
+++ pages/teacher_page/questions/question_photo_delete.php
@@ -1,12 +1,12 @@
 <?php 
 require_once(FUNCTION_DIR . 'preview_function.php');
 
+$teacher_id =isset($_GET['teacher']) ? $_GET['teacher']: '';
 $question_id =isset($_GET['question']) ? $_GET['question']: '';
 $answer_id =isset($_GET['answer']) ? $_GET['answer']: '';
 
 if ($answer_id) {
 	echo $answer_id;
-
 		$getAnswerPhotoId = getAnswerPhotoId($conn, $answer_id);
 		$answerFileName = $getAnswerPhotoId['file_name'];
 		$target_dir = TEST_PHOTO_DIR . $answerFileName ;
@@ -25,26 +25,24 @@ if ($answer_id) {
 			}
 		
 }
-
 else{
+	$getQuestionPhotoId = getQuestionPhotoId($conn, $question_id);
+	$questionFileName = $getQuestionPhotoId['file_name'];
+	$target_dir = TEST_PHOTO_DIR . $questionFileName;
+	if( $questionFileName && is_dir ( $target_dir ) ) {
+		if (!unlink($target_dir)) {
+			echo 'ფოტო( ID -'. $question_id .') არ არის';
 
-$getQuestionPhotoId = getQuestionPhotoId($conn, $question_id);
-$questionFileName = $getQuestionPhotoId['file_name'];
-$target_dir = TEST_PHOTO_DIR . $questionFileName;
-if( $questionFileName && is_dir ( $target_dir ) ) {
-	if (!unlink($target_dir)) {
-		echo 'ფოტო( ID -'. $question_id .') არ არის';
-
-	}else{
-		echo 'წაიშალა';
+		}else{
+			echo 'წაიშალა';
+		}
 	}
-}
-$sql = "DELETE FROM photos_test WHERE item_id=$question_id";
+	$sql = "DELETE FROM photos_test WHERE item_id=$question_id";
 
-if (!mysqli_query($conn, $sql)) {
-	echo "Error updating record: " . mysqli_error($conn);
+	if (!mysqli_query($conn, $sql)) {
+		echo "Error updating record: " . mysqli_error($conn);
+	}
+	echo $questionFileName;
 }
-echo $questionFileName;
-
-}
+header('location:'. BASE_URL . '?mode=view&page=question_preview_edit&question=' . $question_id . "&teacher=" . $teacher_id);
 ?>
